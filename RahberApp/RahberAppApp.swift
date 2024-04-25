d9798ea76ee72072496bc21395e917ee6eab3426
Muneer Kalakuzhiyil <muneerkk66@gmail.com>
2024-04-25 13:51:04 +0100

    Initial Commit

---
+++ RahberApp/RahberAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RahberAppApp.swift
+//  RahberApp
+//
+//  Created by Muneer K K on 25/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct RahberAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
