d1852593c3da19c47a8b7c111f6a45b90aba1a12
Muneer Kalakuzhiyil <muneerkk66@gmail.com>
2024-04-26 11:22:45 +0100

    refactor: update Ui dimensions

---
+++ RahberApp/Presentation/Screens/CourseDetailView/Components/LessonListView.swift
@@ -0,0 +1,18 @@
+//
+//  LessonListView.swift
+//  RahberApp
+//
+//  Created by Muneer K K on 26/04/2024.
+//
+
+import SwiftUI
+
+struct LessonListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LessonListView()
+}
